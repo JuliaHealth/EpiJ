33bcfdc340d837736e941c9cb47946b286518930
Documenter.jl <documenter@juliadocs.github.io>
2022-01-26 05:22:37 +0000

    build based on a832d76

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EpiJ","category":"page"},{"location":"#EpiJ","page":"Home","title":"EpiJ","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EpiJ.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EpiJ]","category":"page"}]
+}
