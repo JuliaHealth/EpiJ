a832d76465fc26a79290039a661c31274b02da6e
TheCedarPrince <jacobszelko@gmail.com>
2022-01-26 00:21:46 -0500

    Fixed links for docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(EpiJ, :DocTestSetup, :(using EpiJ); recursive=true)
 makedocs(;
     modules=[EpiJ],
     authors="Jacob Zelko <jacobszelko@gmail.com>, Georgia Tech Research Institute",
-    repo="https://github.com/JuliaHealth/EpiJ.jl/blob/{commit}{path}#{line}",
-    sitename="EpiJ.jl",
+    repo="https://github.com/JuliaHealth/EpiJ/blob/{commit}{path}#{line}",
+    sitename="EpiJ",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://juliahealth.org/EpiJ.jl",
+        canonical="https://juliahealth.org/EpiJ",
         assets=String[],
     ),
     pages=[
@@ -19,7 +19,7 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/JuliaHealth/EpiJ.jl.git",
+    repo="github.com/JuliaHealth/EpiJ.git",
     devbranch = "main",
     push_preview = false,
     target = "build"
